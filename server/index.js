8d9a9db132eb90a802d6550b20659f5ee93719c1
Ryan <notyuivix@gmail.com>
2026-04-06 21:12:14 -0500

    initial project setup w/ node.js

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('FinTrack server running')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+})
